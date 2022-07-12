4cebfc75b1c6fcf697e8a299d59b67df12dfe00f
Riley Hoffman <rileyjhoff@gmail.com>
2022-07-12 10:37:24 -0700

    completed titleCaseWords challenge

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 function reverseWords(sentence) {
-    let words = sentence.split(' ');
-    for (let i = 0; i < words.length; i++) {
-        const letters = words[i].split('');
-        let newWord = '';
-        for (let i = letters.length - 1; i >= 0; i--) {
-            newWord = newWord.concat(letters[i]);
-        }
-        words[i] = newWord;
+  let words = sentence.split(' ');
+  for (let i = 0; i < words.length; i++) {
+    const letters = words[i].split('');
+    let newWord = '';
+    for (let i = letters.length - 1; i >= 0; i--) {
+      newWord = newWord.concat(letters[i]);
     }
-    words = words.join(' ');
-    return words;
+    words[i] = newWord;
+  }
+  words = words.join(' ');
+  return words;
 }
 
-console.log(reverseWords('alchemy rocks gold'));
+function titleCaseWords(sentence) {
+  const lowerCaseSentence = sentence.toLowerCase();
+  let words = lowerCaseSentence.split(' ');
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    const letters = word.split('');
+    letters[0] = letters[0].toUpperCase();
+    word = letters.join('');
+    words[i] = word;
+  }
+  const newSentence = words.join(' ');
+  return newSentence;
+}
 
-module.exports = { reverseWords };
+console.log(titleCaseWords('alchemy GOLD roCKs'));
+
+module.exports = { reverseWords, titleCaseWords };
--- index.test.js
+++ index.test.js
@@ -1,7 +1,13 @@
-const { reverseWords } = require(".");
+const { reverseWords, titleCaseWords } = require('.');
 
-describe("reverseWords", () => {
-  it("should reverse the words of a sentence", () => {
-    expect(reverseWords("alchemy rocks gold")).toBe("ymehcla skcor dlog");
+describe('reverseWords', () => {
+  it('should reverse the words of a sentence', () => {
+    expect(reverseWords('alchemy rocks gold')).toBe('ymehcla skcor dlog');
+  });
+});
+
+describe('titleCaseWords', () => {
+  it('should title case a sentence', () => {
+    expect(titleCaseWords('alchemy ROCKS goLD')).toBe('Alchemy Rocks Gold');
   });
 });
