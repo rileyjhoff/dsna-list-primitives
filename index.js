d2e81633c55bbc9b2da91353f3ec98d8a530e537
THOMAS HARDING <hardingt604@gmail.com>
2022-07-12 12:26:57 -0500

    add reverseWords to index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function reverseWords(sentence) {}
+function reverseWords(sentence) {
+    let words = sentence.split(' ');
+    for (let i = 0; i < words.length; i++) {
+        const letters = words[i].split('');
+        let newWord = '';
+        for (let i = letters.length - 1; i >= 0; i--) {
+            newWord = newWord.concat(letters[i]);
+        }
+        words[i] = newWord;
+    }
+    words = words.join(' ');
+    return words;
+}
+
+console.log(reverseWords('alchemy rocks gold'));
 
 module.exports = { reverseWords };
